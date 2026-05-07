a256f19f4def5601e90211683f42b547c9f07cbc
Caiqu3wav <asapcaique7@gmail.com>
2026-05-07 15:26:35 -0300

    zipcode fix

--- backendJava/src/main/java/com/dnstore/backend/service/ZipCodeService.java
+++ backendJava/src/main/java/com/dnstore/backend/service/ZipCodeService.java
@@ -1,3 +1,7 @@
+package com.dnstore.backend.service;
+
+import com.dnstore.backend.service.impl.ViaCepResponse;
+
 public interface ZipCodeService {
     /**
      * Valida e recupera informações de endereço para um CEP fornecido.
