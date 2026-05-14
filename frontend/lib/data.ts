9a5849d9161de5b220e99a6c128ef5d5fdb5cd4f
Celsojunior16 <alvarelacjunior@gmail.com>
2026-05-14 09:34:25 -0300

    Update data.ts atualizando as imagens dos produtos

--- frontend/lib/data.ts
+++ frontend/lib/data.ts
@@ -1,18 +1,34 @@
 export const CATEGORIES = [
-  { id: '1', name: 'T-Shirts', image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?auto=format&fit=crop&q=80&w=500', slug: 't-shirts' },
+  { id: '0', name: 'Todos', image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?auto=format&fit=crop&q=80&w=500', slug: 'Todos' },
+  { id: '1', name: 'Camisa Poliamida', image: 'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?auto=format&fit=crop&q=80&w=500', slug: 't-shirts' },
   { id: '2', name: 'Camisas de Ciclismo', image: 'https://images.unsplash.com/photo-1534723452862-4c874018d66d?auto=format&fit=crop&q=80&w=500', slug: 'camisas-ciclismo' },
   { id: '3', name: 'Bonés & Meias', image: 'https://images.unsplash.com/photo-1556821840-3a63f95609a7?auto=format&fit=crop&q=80&w=500', slug: 'acessorios' },
 ];
 
 export const FEATURED_PRODUCTS = [
-  { id: '1', name: 'Camiseta DN Classic', price: 89.90, image: '/assets/products/CaDN.jpg', category: 'T-Shirts' },
-  { id: '2', name: 'Jersey Aero Pro', price: 249.90, image: '/assets/products/CDNBlue.jpg', category: 'Camisas de Ciclismo', tag: 'Novo' },
-  { id: '3', name: 'Camisa Manga Longa MTB', price: 199.90, image: '/assets/products/CDNRed.jpg', category: 'Camisas de Ciclismo' },
-  { id: '4', name: 'Boné Trail Explorer', price: 69.90, image: '/assets/products/BoneTeste.webp', category: 'Acessórios' },
-  { id: '5', name: 'Meia Performance Verde', price: 45.00, image: '/assets/products/TDNBlack.jpg', category: 'Acessórios' },
-  { id: '6', name: 'Tech Tee Respirável', price: 119.90, image: '/assets/products/CamisaTeste.webp', category: 'T-Shirts' },
-  { id: '7', name: 'Tênis Performance', price: 299.90, image: '/assets/products/TenisTeste.webp', category: 'Calçados' },
-];
+  { id: '1', name: 'Camiseta DN Poliamida Cinza', price: 59.99, image: '/assets/products/CaPoliCinza.jpeg', category: 'Camisa Poliamida', color: 'Cinza', size: ["P", "M", "G"] },
+  { id: '2', name: 'Camisa DN Ciclismo Laranja/Azul', price: 199.99, image: '/assets/products/CaCicliLaranja.jpeg', category: 'Camisas de Ciclismo', tag: 'Novo' , color: 'Laranja', size: ["P", "M", "G"] },
+  { id: '3', name: 'Boné DN Preto/Vermelho', price: 59.99, image: '/assets/products/BoneDNPreto.jpeg', category: 'Bonés & Meias' , color: 'Preto e Vermelho', size: ["35", "40", "42"] },
+  { id: '4', name: 'Meia DN Preto/Vermelho', price: 49.99, image: '/assets/products/MeiaDNPreto.jpeg', category: 'Bonés & Meias' , color: 'Preto e Vermelho', size: ["35", "40", "42"] },
+  { id: '5', name: 'Camiseta DN Poliamida Azul', price: 59.99, image: '/assets/products/CaPoliAzul.jpeg', category: 'Camisa Poliamida' , color: 'Azul', size: ["P", "M", "G"] },
+  { id: '6', name: 'Camisa DN Ciclismo Azul', price: 179.90, image: '/assets/products/CaCicliAzul.jpeg', category: 'Camisas de Ciclismo' , color: 'Azul', size: ["P", "M", "G"] },
+  { id: '7', name: 'Camisa DN Poliamida Laranja/Azul', price:59.90, image: '/assets/products/CaPoliLaranja.jpeg', category: 'Camisa Poliamida' , color: 'Laranja', size: ["P", "M", "G"] },
+  { id: '8', name: 'Camisa DN Ciclismo Marrom', price:229.99, image: '/assets/products/CaCicliMarrom.jpeg', category: 'Camisas de Ciclismo' , color: 'Marrom', size: ["P", "M", "G"] },
+  { id: '9', name: 'Camisa DN Ciclismo Vermelha', price:179.90, image: '/assets/products/CaCicliVermelha.jpeg', category: 'Camisas de Ciclismo' , color: 'Vermelha', size: ["P", "M", "G"] },
+  { id: '10', name: 'Camisa DN Poliamida Rosa Choque Escuro', price:59.99, image: '/assets/products/CaPoliVinho.jpeg', category: 'Camisa Poliamida' , color: 'Rosa', size: ["P", "M", "G"] },
+  { id: '11', name: 'Camisa DN Ciclismo Preta', price:179.90, image: '/assets/products/CaCicliPreto.jpeg', category: 'Camisas de Ciclismo' , color: 'Preta', size: ["P", "M", "G"] },
+  { id: '12', name: 'Camisa DN Ciclismo Marrom/Amarela', price:179.90, image: '/assets/products/CaCicliMarrelo.jpeg', category: 'Camisas de Ciclismo' , color: 'Marrom', size: ["P", "M", "G"] },
+  { id: '13', name: 'Camiseta DN Poliamida Preta', price: 59.99, image: '/assets/products/CaPoliPreto.jpeg', category: 'Camisa Poliamida' , color: 'Preta', size: ["P", "M", "G"] },
+  { id: '14', name: 'Camiseta DN Poliamida Branca', price: 59.99, image: '/assets/products/CaPoliBranca.jpeg', category: 'Camisa Poliamida' , color: 'Branca', size: ["P", "M", "G"] },
+  { id: '15', name: 'Camiseta DN Poliamida Preta/Amarelo', price: 59.99, image: '/assets/products/CaPoliPremerelo.jpeg', category: 'Camisa Poliamida' , color: 'Preta', size: ["P", "M", "G"] },
+  { id: '16', name: 'Camiseta DN Poliamida Azul/Amarelo', price: 59.99, image: '/assets/products/CaPoliAzurelo.jpeg', category: 'Camisa Poliamida' , color: 'Azul', size: ["P", "M", "G"] },
+  { id: '17', name: 'Camiseta DN Poliamida Vermelha', price: 59.99, image: '/assets/products/CaPoliVermelha.jpeg', category: 'Camisa Poliamida' , color: 'Vermelha', size: ["P", "M", "G"] },
+  { id: '18', name: 'Camiseta DN Poliamida Cinza/Verde', price: 59.99, image: '/assets/products/CaPoliCinzaVerde.jpeg', category: 'Camisa Poliamida' , color: 'Cinza', size: ["P", "M", "G"] },
+  { id: '19', name: 'Camiseta DN Poliamida Preta/Laranja', price: 59.99, image: '/assets/products/CaPoliPretaLaranja.jpeg', category: 'Camisa Poliamida' , color: 'Preta', size: ["P", "M", "G"] },
+  { id: '20', name: 'Camiseta DN Poliamida Preta/Roxo', price: 59.99, image: '/assets/products/CaPoliPretaRoxa.jpeg', category: 'Camisa Poliamida' , color: 'Preta', size: ["P", "M", "G"] },
+  { id: '21', name: 'Camiseta DN Poliamida Branca/Vermelha', price: 59.99, image: '/assets/products/CaPoliBrancoVermelha.jpeg', category: 'Camisa Poliamida' , color: 'Branca', size: ["P", "M", "G"] },
+  { id:'22', name: 'Boné DN Preto/Verde', price: 59.99, image: '/assets/products/BonePretaVerde.jpeg', category: 'Bonés & Meias' , color: 'Preto e Verde', size: ["35", "40", "42"] },
+  ];
 
 export const EVENTS = [
   {
